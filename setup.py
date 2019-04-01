ac6324e44bca870634172b6f113258ffb70ab327
Palani <vasudevan.palani@gmail.com>
2019-04-01 14:40:55 -0400

    Updated long esc

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import setup
-
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 setup(
     name='pspring-aws',
-    version='0.0.13',
+    version='0.0.14',
     license='TBD',
     author='Vasudevan Palani',
     author_email='vasudevan.palani@gmail.com',
     url='https://github.com/vasudevan-palani/pspring-aws',
-    long_description="README.md",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=['pspringaws'],
     install_requires=['boto3>1.9','pspring>=0.0.1'],
     include_package_data=True,
