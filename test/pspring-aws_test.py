ef7b38f1e5d3dec6a6d1fdd76c8558c03ba3649e
Palani <vasudevan.palani@gmail.com>
2019-05-09 18:33:03 -0400

    Updated for realtime dynamodbconfig

--- test/pspring-aws_test.py
+++ test/pspring-aws_test.py
@@ -13,37 +13,61 @@ import pspringaws
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
-def test_realtimesecretmngr():
-    configProvider = pspringaws.RealTimeSecretsMgrConfigProvider(secretId=secretId)
-    assert configProvider.getProperty("XX")==str(3600)
+# def test_realtimesecretmngr():
+#     configProvider = pspringaws.RealTimeSecretsMgrConfigProvider(secretId=secretId)
+#     assert configProvider.getProperty("XX")==str(3600)
 
-def test_secretmngr():
-    configProvider = pspringaws.SecretsMgrConfigProvider(secretId=secretId)
-    assert configProvider.getProperty("XX")==str(3600)
+# def test_secretmngr():
+#     configProvider = pspringaws.SecretsMgrConfigProvider(secretId=secretId)
+#     assert configProvider.getProperty("XX")==str(3600)
 
-def test_dynamodb():
+# def test_dynamodb():
 
-    @pspringaws.DynamoDBTable(tableName="XXX",primaryKey="name",sortKey="scope")
-    class TestDynamo():
-        pass
-    table = TestDynamo()
-    response = table.get("test",column="firstname",scope="test")
-    assert response=="vas"
+#     @pspringaws.DynamoDBTable(tableName="XXX",primaryKey="name",sortKey="scope")
+#     class TestDynamo():
+#         pass
+#     table = TestDynamo()
+#     response = table.get("test",column="firstname",scope="test")
+#     assert response=="vas"
 
-def test_s3configprovider():
-    configProvider = pspringaws.S3ConfigProvider(bucketId="XXX",objectKey="XXX")
-    assert configProvider.getProperty("XXX")!=None
+def test_realtimedynamoconfigprovider():
+    configProvider = pspringaws.RealTimeDynamodbConfigProvider(
+        tableName="XXX",
+        primaryKey="XXX",
+        primaryKeyName="XXX",
+        sortKeyName="XXX",
+        sortKey="XX",
+        configColumnName="data",
+        apiId="XXX"
+    )
+    time.sleep(20)
+    assert configProvider.getProperty("firstname") == "vasudevan"
 
-def test_realtimes3configprovider():
-    configProvider = pspringaws.RealTimeS3ConfigProvider(bucketId="XXX",objectKey="XXX")
-    assert configProvider.getProperty("XXX")!=None
+# def test_dynamoconfigprovider():
+#     configProvider = pspringaws.DynamodbConfigProvider(
+#         tableName="XXX",
+#         primaryKey="XXX",
+#         primaryKeyName="XX",
+#         sortKeyName="XXX",
+#         sortKey="XXX",
+#         configColumnName="data"
+#     )
+#     assert configProvider.getProperty("visitorId")!=None
 
-def test_scheduleds3configprovider():
-    configProvider = pspringaws.ScheduledS3ConfigProvider(bucketId="XXX",objectKey="XXX",period="5")
-    time.sleep(50)
-    assert configProvider.getProperty("sooproxyapi.apiconfig")!=None
+# def test_s3configprovider():
+#     configProvider = pspringaws.S3ConfigProvider(bucketId="XXX",objectKey="XXX")
+#     assert configProvider.getProperty("XXX")!=None
 
-def test_scheduledsecretmngrconfigprovider():
-    configProvider = pspringaws.ScheduledSecretsMngrConfigProvider(secretId="XXX",period="5")
-    time.sleep(50)
-    assert configProvider.getProperty("sooproxyapi.apiconfig")!=None
+# def test_realtimes3configprovider():
+#     configProvider = pspringaws.RealTimeS3ConfigProvider(bucketId="XXX",objectKey="XXX")
+#     assert configProvider.getProperty("XXX")!=None
+
+# def test_scheduleds3configprovider():
+#     configProvider = pspringaws.ScheduledS3ConfigProvider(bucketId="XXX",objectKey="XXX",period="5")
+#     time.sleep(50)
+#     assert configProvider.getProperty("sooproxyapi.apiconfig")!=None
+
+# def test_scheduledsecretmngrconfigprovider():
+#     configProvider = pspringaws.ScheduledSecretsMngrConfigProvider(secretId="XXX",period="5")
+#     time.sleep(50)
+#     assert configProvider.getProperty("sooproxyapi.apiconfig")!=None
