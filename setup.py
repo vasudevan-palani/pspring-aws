0e426baa25bd10a69f5942b8b148e3e6c9d92d78
Vasu <vasudevan.palani@gmail.com>
2019-10-31 14:36:34 -0400

    bumped version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 setup(
     name='pspring-aws',
-    version='0.0.30',
+    version='0.0.31',
     license='TBD',
     author='Vasudevan Palani',
     author_email='vasudevan.palani@gmail.com',
